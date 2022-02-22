e150aedcf7657b971dc6d5544a5c0c3823bc2e97
Dorothea von Randow <thearandow@gmail.com>
2022-02-22 18:24:17 +0100

    first steps for the dashboard

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,50 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: :destroy
+  before_action :set_buddy, only: [:new, :create]
+
+  def index
+    @bookings = Booking.all
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @booking = User.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.buddy = @booking
+    flash[:notice] = @booking.errors.full_messages.to_sentence unless @booking.save
+    redirect_to buddy_path(@buddy)
+  end
+
+  def edit
+    @booking = Booking
+  end
+
+  def update
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to list_path(@booking.list)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:request_message, :date, :buddy_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_buddy
+    @buddy = Buddy.find(params[:buddy_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying the buddies..."
+Buddy.destroy_all
+puts "Buddies destroyed ! 😭"
+
+puts "..."
+
+puts "Destroying the user..."
+User.destroy_all
+puts "Users destroyed ! 😭"
+
+puts "..."
+
+puts "Destroying the bookings..."
+Booking.destroy_all
+puts "Bookings destroyed ! 😭"
+
+puts "..."
+
+puts "Creating 20 super true buddies..."
+20.times do
+  buddy = Buddy.new(name: ['Antoine', 'Pierre', 'Léa', 'Lizzie', 'Dorothea', 'David', 'Julie', 'Mélanie', 'Jésus',
+                           'Aleksander', 'Helmut', 'Louis', 'Pierre-Emmanuel'].sample,
+                    age: rand(18..99),
+                    gender: Faker::Gender.type,
+                    city: ['Berlin', 'Hamburg', 'Leipzig', 'Postdam', 'Munich'].sample,
+                    biography: Faker::Lorem.sentence(word_count: 50),
+                    hobby: [Faker::Hobby.activity, Faker::Hobby.activity],
+                    language: ['German', 'French', 'English', 'Chinese', 'Hungarian', 'Greek', 'Russian', 'Angolan',
+                               'Ethiopian', 'Arabic'].sample(2))
+  buddy.save!
+end
+puts "Buddies created 👯"
+
+puts "..."
+
+puts "Creating 5 super true users..."
+User.create!(email: 'user1@example.com', password: 'user1!', username: 'Thea')
+User.create!(email: 'user2@example.com', password: 'user2!')
+User.create!(email: 'user3@example.com', password: 'user3!')
+User.create!(email: 'user4@example.com', password: 'user4!')
+User.create!(email: 'user5@example.com', password: 'user5!')
+puts "Users created 💃"
+
+puts "Creating 13 super true bookings..."
+13.times do
+  booking = Booking.new(user_id: 1, # can I even do this manually?
+                        buddy_id: 1, # same as obove?
+                        date: 2022-05-12, # apprarently not the right format
+                        request_message: "This is my request message. I hope it is long enough!")
+  booking.save!
+end
+puts "Booking created created 👯"
