ba67a020bbe630da75f2e7167e05e39321c6fee1
lizzie lerwill <lizzielerwill@hotmail.com>
2022-02-22 14:03:53 +0100

    changed some views but need buddies conroller

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,29 @@ ActiveRecord::Schema.define(version: 2022_02_21_154615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "date"
+    t.text "request_message"
+    t.bigint "user_id", null: false
+    t.bigint "buddy_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["buddy_id"], name: "index_bookings_on_buddy_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "buddies", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "gender"
+    t.string "city"
+    t.text "biography"
+    t.text "hobby"
+    t.string "language"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,4 +51,6 @@ ActiveRecord::Schema.define(version: 2022_02_21_154615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "buddies"
+  add_foreign_key "bookings", "users"
 end
