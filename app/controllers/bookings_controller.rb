5a42fcd528769b8e6bb97beba57c1b0cc5a4dd9b
David Morio <david.morio.lesavourey@gmail.com>
2022-02-22 16:15:16 +0100

    Booking new and create implemented

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,6 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: :destroy
   before_action :set_buddy, only: [:new, :create]
 
-  def index
-    @bookings = Booking.all
-    @user = User.find(params[:id])
-  end
-
-  def show
-    @booking = User.find(params[:id])
-  end
 
   def new
     @booking = Booking.new
@@ -17,31 +8,18 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.buddy = @booking
-    flash[:notice] = @booking.errors.full_messages.to_sentence unless @booking.save
-    redirect_to buddy_path(@buddy)
-  end
-
-  def edit
-    @booking = Booking
-  end
-
-  def update
-  end
-
-  def destroy
-    @booking.destroy
-    redirect_to list_path(@booking.list)
+    @booking.buddy = @buddy
+    if @booking.save
+      redirect_to buddy_path(@buddy)
+    else
+      render :new
+    end
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:request_message, :date, :buddy_id)
-  end
-
-  def set_booking
-    @booking = Booking.find(params[:id])
   end
 
   def set_buddy
